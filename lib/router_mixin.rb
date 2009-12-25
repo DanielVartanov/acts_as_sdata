abc1b592cf54551a2dcebec171f56b2683ef948d
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-12-25 11:16:18 +0600

    An option to remove ambiguity in collection path

--- lib/router_mixin.rb
+++ lib/router_mixin.rb
@@ -1,13 +1,27 @@
 module SData
   module RouterMixin
-    def sdata_resource(name)
+    def sdata_resource(name, options={})
       pluralized_name = name.to_s.pluralize
-      map_sdata_collection(pluralized_name)
+      formatted_collection_path = options.delete :formatted_collection_path
+
+      formatted_collection_path ?
+        map_formatted_sdata_collection(pluralized_name) :
+        map_sdata_collection(pluralized_name)
+
+      map_sdata_collection_with_predicate(pluralized_name)
       map_sdata_instance(pluralized_name)
     end
 
+    def map_formatted_sdata_collection(pluralized_name)
+      connect "/#{pluralized_name}.sdata", :controller => pluralized_name, :action => 'sdata_collection'
+    end
+
     def map_sdata_collection(pluralized_name)
-      connect "/#{pluralized_name}(\\(:predicate\\))", :controller => pluralized_name, :action => 'sdata_collection'
+      connect "/#{pluralized_name}", :controller => pluralized_name, :action => 'sdata_collection'
+    end
+
+    def map_sdata_collection_with_predicate(pluralized_name)
+      connect "/#{pluralized_name}\\(:predicate\\)", :controller => pluralized_name, :action => 'sdata_collection'
     end
 
     def map_sdata_instance(pluralized_name)
