5b69e57591150d29ba875a7400249c589b43f007
Daniel Vartanov <daniel.vartanov@gmail.com>
2010-01-24 16:40:52 +0600

    HTTP steps for Cucumber

---
+++ test/functional/proving-ground-app/features/step_definitions/http_steps.rb
@@ -0,0 +1,28 @@
+When /^I get (.+)$/ do |page_address|
+  get page_address, nil
+end
+
+When /^I post to (.+) with:$/ do |page_address, data|
+  header 'Content-Type', Webrat::MIME.mime_type('.xml')
+  request_page page_address, :post, data
+end
+
+Then /^response body should have XPath (.+)$/ do |xpath|
+  xml_document = Nokogiri::XML(response.body.to_s)
+  xml_node_set = xml_document.root.children
+  xml_node_set.should have_xpath(xpath)
+end
+
+Then /^response body should not have XPath (.*)$/ do |xpath|
+  xml_document = Nokogiri::XML(response.body.to_s)
+  xml_node_set = xml_document.root.children
+  xml_node_set.should_not have_xpath(xpath)
+end
+
+Then /response status should be (\d+)/ do |code|
+  response.code.to_i.should == code.to_i
+end
+
+Then /^show me the output$/ do
+  puts response_body
+end
