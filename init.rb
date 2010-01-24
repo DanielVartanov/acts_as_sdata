ebd9e0293c76d1c6ae945b9cf98fe23f773e6365
Daniel Vartanov <daniel.vartanov@gmail.com>
2010-01-24 17:03:08 +0600

    Initializing acts_as_sdata in prooving-ground-app

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'activesupport'
-require 'atom' # TODO: add ratom dependency
+require 'atom' # TODO: add ratom _dependency_
 
 __DIR__ = File.dirname(__FILE__)
 Dir.glob(File.join(__DIR__, 'lib', '**', '*.rb')) { |filename| require filename }
---
+++ test/functional/proving-ground-app/vendor/plugins/acts_as_sdata/init.rb
@@ -0,0 +1,5 @@
+__DIR__ = File.dirname(__FILE__)
+
+genuine_init_file = File.join __DIR__, '..', '..', '..', '..', '..', '..', 'init'
+
+require genuine_init_file
