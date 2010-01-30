952c5693b65beb82a3666725f6b158fea8218dac
Daniel Vartanov <daniel.vartanov@gmail.com>
2010-01-30 17:13:46 +0600

    Removing REST routes

--- test/functional/proving-ground-app/config/routes.rb
+++ test/functional/proving-ground-app/config/routes.rb
@@ -1,12 +1,3 @@
 ActionController::Routing::Routes.draw(:delimiters => ['/', '.', '!', '\(', '\)' ]) do |map|
   map.sdata_resource :items
-
-#  map.connect '/items/create', :controller => 'items', :action => 'create'
-  map.connect '/items/:id', :controller => 'items', :action => 'show'
-
-#  map.connect '/items', :controller => 'items', :action => 'index', :conditions => { :method => :get }
-  map.connect '/items', :controller => 'items', :action => 'create', :conditions => { :method => :post }
-
-#  map.connect ':controller/:action/:id'
-#  map.connect ':controller/:action/:id.:format'
 end
