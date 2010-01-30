6975d5102b7c5a11699cf296332f12f18166afac
Daniel Vartanov <daniel.vartanov@gmail.com>
2010-01-30 17:39:08 +0600

    Atom entry recognition in HTTP request body

--- test/functional/proving-ground-app/config/initializers/mime_types.rb
+++ test/functional/proving-ground-app/config/initializers/mime_types.rb
@@ -1,5 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register_alias "text/html", :iphone
+ActionController::Base.param_parsers[Mime::Type.lookup('application/atom+xml')] = Proc.new do |data|
+  { :entry => Atom::Entry.load_entry(data) }
+end
