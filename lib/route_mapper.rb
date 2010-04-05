976a2cb144a0ee1c198235c4bb452d49fb1f6b52
Daniel <daniel@daniel-workstation.(none)>
2010-04-05 15:56:39 +0600

    Path prefix for sdata_resource URL. Also refactored

---
+++ lib/route_mapper.rb
@@ -0,0 +1,59 @@
+module SData
+  class RouteMapper < Struct.new(:router, :resource_name, :options)
+    def map_sdata_routes!
+      map_sdata_collection
+      map_sdata_collection_with_predicate
+      map_sdata_show_instance
+      map_sdata_create_instance
+      map_sdata_update_instance
+    end
+
+  protected
+
+    def map_sdata_collection
+      map_route "#{name_in_path}", 'sdata_collection', :get
+    end
+
+    def map_sdata_collection_with_predicate
+      map_route "#{name_in_path}\\(:predicate\\)", 'sdata_collection', :get
+    end
+
+    def map_sdata_show_instance
+      map_route "#{name_in_path}/!:instance_id", 'sdata_show_instance', :get
+    end
+
+    def map_sdata_create_instance
+      map_route "#{name_in_path}", 'sdata_create_instance', :post
+    end
+
+    def map_sdata_update_instance
+      map_route "#{name_in_path}/!:instance_id", 'sdata_update_instance', :put
+    end
+
+    def map_route(path, action, method)
+      path = prefix + path if prefix?
+      path = path + ".sdata" if formatted_paths?
+      router.connect path, :controller => controller, :action => action, :conditions => { :method => method }
+    end
+
+    def controller
+      @controller ||= resource_name.to_s.pluralize
+    end
+
+    def name_in_path
+      @name_in_path ||= resource_name.to_s.camelize.pluralize
+    end
+
+    def formatted_paths?
+      @formatted_paths ||= options[:formatted_paths]
+    end
+
+    def prefix
+      @prefix ||= (options[:prefix] || '/')
+    end
+
+    def prefix?
+      !! prefix
+    end
+  end
+end
--- lib/router_mixin.rb
+++ lib/router_mixin.rb
@@ -1,39 +1,8 @@
 module SData
   module RouterMixin
     def sdata_resource(name, options={})
-      pluralized_name = name.to_s.pluralize
-      formatted_paths = options.delete :formatted_paths
-
-      map_sdata_collection(pluralized_name, formatted_paths)
-      map_sdata_collection_with_predicate(pluralized_name)
-      map_sdata_show_instance(pluralized_name)
-      map_sdata_create_instance(pluralized_name, formatted_paths)
-      map_sdata_update_instance(pluralized_name)
-    end
-
-    def map_sdata_collection(pluralized_name, formatted_paths = false)
-      path = formatted_paths ? "/#{pluralized_name}.sdata" : "/#{pluralized_name}"
-      connect path, :controller => pluralized_name, :action => 'sdata_collection', :conditions => { :method => :get }
-    end
-
-    def map_sdata_collection_with_predicate(pluralized_name)
-      path = "/#{pluralized_name}\\(:predicate\\)"
-      connect path, :controller => pluralized_name, :action => 'sdata_collection', :conditions => { :method => :get }
-    end
-
-    def map_sdata_show_instance(pluralized_name)
-      path = "#{pluralized_name}/!:instance_id"
-      connect path, :controller => pluralized_name, :action => 'sdata_show_instance', :conditions => { :method => :get }
-    end
-
-    def map_sdata_create_instance(pluralized_name, formatted_paths = false)
-      path = formatted_paths ? "/#{pluralized_name}.sdata" : "/#{pluralized_name}"
-      connect path, :controller => pluralized_name, :action => 'sdata_create_instance', :conditions => { :method => :post }
-    end
-
-    def map_sdata_update_instance(pluralized_name)
-      path = "#{pluralized_name}/!:instance_id"
-      connect path, :controller => pluralized_name, :action => 'sdata_update_instance', :conditions => { :method => :put }
+      route_creator = SData::RouteMapper.new(self, name, options)
+      route_creator.map_sdata_routes!
     end
   end
 end
