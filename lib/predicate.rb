ab9a3db8fc52fd019b1173ef1e290c7ce4edaf42
Daniel <daniel@daniel-workstation.(none)>
2010-04-10 21:15:25 +0600

    Minor refactoring

--- lib/predicate.rb
+++ lib/predicate.rb
@@ -1,8 +1,7 @@
 module SData
   class Predicate < Struct.new(:field, :relation, :value)
     def self.parse(predicate_string)
-      match_data = predicate_string.match(/(\w+)\s(gt|lt|eq)\s([\w\d]+)/)
-      match_data ||= []
+      match_data = predicate_string.match(/(\w+)\s(gt|lt|eq)\s([\w\d]+)/) || []
       self.new match_data[1], match_data[2], match_data[3]
     end
 
