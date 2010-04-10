d2b6fc487dbdcad8cf8fa148254326d54d7912cc
Daniel <daniel@daniel-workstation.(none)>
2010-04-10 21:17:52 +0600

    Adding namespaces to Atom Feed (a very first draft)

--- lib/controller_mixin.rb
+++ lib/controller_mixin.rb
@@ -12,7 +12,7 @@ module SData
         def sdata_collection
           collection = build_sdata_feed
           collection.entries += sdata_scope.map(&:to_atom)
-
+          
           render :xml => collection, :content_type => "application/atom+xml; type=feed"
         end
 
@@ -54,12 +54,13 @@ module SData
         end
 
         def build_sdata_feed
+          Namespace.add_feed_extension_namespaces(%w{sdata http opensearch sle xsi})
           Atom::Feed.new do |f|
             f.title = sdata_options[:feed][:title]
             f.links << Atom::Link.new(:href => 'http://example.com' + sdata_options[:feed][:path])
             f.updated = Time.now
             f.authors << Atom::Person.new(:name => sdata_options[:feed][:author])
-            f.id = sdata_options[:feed][:id]
+            f.id = request.protocol + request.host_with_port + request.request_uri
           end
         end
 
