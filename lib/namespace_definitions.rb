ac53adc2a4a5422775391b3b1a613597ad2c2ead
eugene gilburg <eugene@richard-test2.(none)>
2010-04-23 16:08:11 -0700

    moving namespaces to env.rb (should later be in application.yml)

--- lib/namespace_definitions.rb
+++ lib/namespace_definitions.rb
@@ -1,22 +1,12 @@
 module SData
   class Namespace
-    #TODO: yaml file is probably better for the definitions
-    @@sdata_schemas = { 
-                           "crmErp"     => "http://schemas.sage.com/crmErp",
-                           "http"       => "http://schemas.sage.com/sdata/http/2008/1",
-                           "opensearch" => "http://a9.com/-/spec/opensearch/1.1",
-                           "sdata"      => "http://schemas.sage.com/sdata/2008/1",
-                           "sle"        => "http://www.microsoft.com/schemas/rss/core/2005",
-                           "xsi"        => "http://www.w3.org/2001/XMLSchema-instance"
-                         }
-                         
     def self.sdata_schemas
-      @@sdata_schemas
+      $SDATA_SCHEMAS
     end
     
     def self.add_feed_extension_namespaces(namespaces)
       namespaces.each do |namespace|
-         Atom::Feed.add_extension_namespace(namespace.to_s, @@sdata_schemas[namespace.to_s])
+         Atom::Feed.add_extension_namespace(namespace.to_s, self.sdata_schemas[namespace.to_s])
       end
     end
 
