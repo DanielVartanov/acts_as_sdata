256abd88538b6aa09496f207c08ee42df15e4733
eugene gilburg <eugene@richard-test2.(none)>
2010-04-23 16:12:07 -0700

    adding global var (Should later be in application.yml)

--- init.rb
+++ init.rb
@@ -2,5 +2,14 @@ require 'active_support'
 require 'action_controller'
 require 'atom' # TODO: add ratom _dependency_
 
+  $SDATA_SCHEMAS = { 
+                     "crmErp"     => "http://schemas.sage.com/crmErp",
+                     "http"       => "http://schemas.sage.com/sdata/http/2008/1",
+                     "opensearch" => "http://a9.com/-/spec/opensearch/1.1",
+                     "sdata"      => "http://schemas.sage.com/sdata/2008/1",
+                     "sle"        => "http://www.microsoft.com/schemas/rss/core/2005",
+                     "xsi"        => "http://www.w3.org/2001/XMLSchema-instance"
+                   }
+
 __DIR__ = File.dirname(__FILE__)
 Dir.glob(File.join(__DIR__, 'lib', '**', '*.rb')) { |filename| require filename }
