12427d73c8ac83e43b1b17fa38c733d48b49ecb1
DanielVartanov <daniel.vartanov@gmail.com>
2010-09-22 18:24:57 +0600

    Specs for #has_sdata_options

--- spec/controller_mixin/has_sdata_options_spec.rb
+++ spec/controller_mixin/has_sdata_options_spec.rb
@@ -1,8 +1,9 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+__DIR__ = File.dirname(__FILE__)
+require File.join(__DIR__, '..', 'spec_helper')
 
 include SData
 
-describe ControllerMixin, ".acts_as_sdata" do
+describe ControllerMixin, ".has_sdata_options" do
   describe "given an ActionController::Base class extended by ControllerMixin" do
     before :all do
       Base = Class.new ActionController::Base
@@ -11,7 +12,7 @@ describe ControllerMixin, ".acts_as_sdata" do
 
     before :each do
       @options = { :model => Class.new }
-      Base.acts_as_sdata @options
+      Base.has_sdataoptions @options
     end
 
     it "should make passed options available for class" do
